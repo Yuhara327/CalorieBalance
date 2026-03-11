a739a93a3a8986c67f166031db792b253ec812be
yuhara327 <so960397@icloud.com>
2026-03-12 01:13:09 +0900

    initial commit

---
+++ CalorieBalance/Models/CalorieData.swift
@@ -0,0 +1,22 @@
+//
+//  CalorieData.swift
+//  CalorieBalance
+//
+//  Created by Soichiro Yuhara on 2026/03/11.
+//
+
+import Foundation
+
+struct CalorieData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let activeCalories: Double
+    let restingCalories: Double
+    let dietaryCalories: Double
+    var totalCosumedCalories: Double {
+        return activeCalories + restingCalories
+    }
+    var netCalories: Double{
+        return totalCosumedCalories - dietaryCalories
+    }
+}
---
+++ CalorieBalance/Services/HealthKitManager.swift
@@ -0,0 +1,13 @@
+//
+//  HealthKitManager.swift
+//  CalorieBalance
+//
+//  Created by Soichiro Yuhara on 2026/03/11.
+//
+import HealthKit
+
+class HealthKitManager {
+    private let healthStore = HKHealthStore()
+    
+    
+}
