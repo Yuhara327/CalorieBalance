91d8a716a1ab6f1788405d10dfd7fc164b0a876a
yuhara327 <so960397@icloud.com>
2026-03-11 23:42:25 +0900

    Initial Commit

---
+++ CalorieBalance/CalorieBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieBalanceApp.swift
+//  CalorieBalance
+//
+//  Created by Soichiro Yuhara on 2026/03/11.
+//
+
+import SwiftUI
+
+@main
+struct CalorieBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieBalance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalorieBalance
+//
+//  Created by Soichiro Yuhara on 2026/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
